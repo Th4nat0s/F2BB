f9c007d34d3a2dc407feebb957a9fcc95788db69
Thanat0s <thanspam@trollprod.org>
2013-06-22 18:46:30 +0200

    time format and action escaping

--- f2bb.py
+++ f2bb.py
@@ -17,7 +17,7 @@
 # along with Fail2Ban BroadCast; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-import time,sys,re,socket,subprocess,traceback,argparse
+import time,datetime,sys,re,socket,subprocess,traceback,argparse
 
 try:
   import hashlib
@@ -46,7 +46,7 @@ INIFILE = "/etc/cfg-f2bb.conf"
 # Don't touch it... if you don't know why
 host = ''
 delim = ";" 
-version = '0.3'
+version = '0.3.1'
 header = "F2BB"
 mdelay = 0.75  # maxtime of message in sec
 verbose = False
@@ -74,6 +74,7 @@ def okproto(param):
   else:
     return False
 
+# Validate port xx , xx,xx or xx-xx
 def okport(param):
   if re.search(r'^\d{1,5}[-:,]\d{1,5}$|^\d{1,5}(,\d{1,5}){0,}$',param):
     return True
@@ -93,7 +94,7 @@ def okaction(param):
     return False
 
 def prtlog(arg):
-  logc = log.replace('<timestamp>',time.ctime(time.time()) )
+  logc = log.replace('<timestamp>', datetime.datetime.utcnow().isoformat(' '))
   logc = logc.replace('<mode>',arg['mode'])
   logc = logc.replace('<ip_src>',arg['ip_src'])
   logc = logc.replace('<port>',arg['port'])
@@ -135,7 +136,7 @@ def func_send(arg):
   try:
     s.sendto(header+sign(message,password)+";"+message, (broadcast, port ))
     dbgprint ("Packet sent to " + broadcast + ':' + str(port))
-    if int(logtype) and 1: 
+    if int(logtype) & 1: 
       prtlog({'mode': 'client' , 'ip_src': arg.ip_src, 'port': arg.port, 'protocol': arg.protocol, 'jail_name': arg.jail_name, 'client_name': arg.client_name , 'action':arg.action, 'ip_dst': arg.ip_dst})
   except:
     dbgprint ("Error sending packet")
@@ -148,6 +149,12 @@ def goodhash(lhash,lpayload,lpassword):
   else:
     return False
 
+def escapestr(instr):
+  instr = instr.replace('"','\"')
+  instr = instr.replace("'","\'")
+  instr = instr.replace('\\','\\\\')
+  return (instr)
+
 # Receive program
 def func_recv(args):
   #print args.verbose
@@ -157,7 +164,7 @@ def func_recv(args):
   s.bind((host, port))
 
   dbgprint ("Daemon Started...")
-  while 1: # Endless Loop
+  while True: # Endless Loop
     try:
       payload, address = s.recvfrom(8192)
       dbgprint ("Packet received")
@@ -182,7 +189,7 @@ def func_recv(args):
               daction = daction.replace('<client_name>',client)  #    Why used ??
               daction = daction.replace('<ip_dst>',dip)
               subprocess.call(daction.split(' '))   # Execute the action
-              if int(logtype) and 2: 
+              if int(logtype) & 2: 
                 prtlog({'mode': 'daemon' , 'ip_src': ip, 'port': fport, 'protocol': proto, 'jail_name': jail, 'client_name': client , 'action':action, 'ip_dst': dip})
             else:
               dbgprint("Invalid data")
@@ -215,7 +222,7 @@ def getparm(obj,section,oconf):
 
 def dbgprint(mystr):
   if verbose:
-    print ("%s %s") % (time.ctime(time.time()),mystr)
+    print ("%s %s") % (datetime.datetime.utcnow().isoformat(' '),mystr)
 
 def init():
   parser = argparse.ArgumentParser( description='F2BB v'+version+' (c) Thanatos 2013', usage='Broadcast Fail2Ban updates to a pool\n')
@@ -242,7 +249,7 @@ def init():
     verbose = True
 
   global header,port,password,broadcast,action_ban,action_uban,log,logtype,logfile
-  vmaj,vmin = version.split('.')
+  vmaj,vmin,vsub = version.split('.')
   header = header + chr(int(vmaj)) + chr(int(vmin))
 
   # Read config File
@@ -256,8 +263,10 @@ def init():
     enderror('Invalid port configuration')
   broadcast = getparm(CONFIG,section,'broadcast')
   password = getparm(CONFIG,section,'password')
-  action_ban = getparm(CONFIG,section,'action_ban')
-  action_uban = getparm(CONFIG,section,'action_uban')
+  action_ban = escapestr( getparm(CONFIG,section,'action_ban'))
+  action_uban = escapestr(getparm(CONFIG,section,'action_uban'))
+
+  
 
   log = getparm(CONFIG,section,'log')
   logfile = getparm(CONFIG,section,'logfile')
